4cc56f8553ced0dd08da5781af140306e56f7a75
Tiagoytsk <tiagompedra@gmail.com>
2024-05-15 18:19:07 +0100

    ex2

--- Exercicio2.cpp
+++ Exercicio2.cpp
@@ -18,15 +18,19 @@ public:
 
 int main(){
     char tipoaposta, gerar;
-    int nchaves, nnumap, numsapostados[10];
-    do{
+    int nchaves,apestrela,apnum, numap,chaap, numsapostados[10],estrelasapostados[2];
+    numap = 5;
+    chaap=2;
+
+    do {
         cout << "Bem vindo ao Euromilhões! \nEscolha o tipo de aposta que deseja realizar: \n Aposta Simples (carregue S) \n Aposta Multipla (carregue M)";
         cin >> tipoaposta;
-    }
-    while (tipoaposta!='S' || tipoaposta!='s' || tipoaposta!='M' || tipoaposta!='m');
-    
+        cout << tipoaposta;
+    } 
+    while (tipoaposta != 'S' && tipoaposta != 's' && tipoaposta != 'M' && tipoaposta != 'm');
+
     if (tipoaposta=='S' || tipoaposta=='s')
-    {
+       {
         cout << "Escolheu a Aposta Simples!\nQuantos boletins pertende apostar?";
         cin >> nchaves;
         do{
@@ -34,23 +38,45 @@ int main(){
             cout << "Aposta Gerada Automaticamente (Carregue A) ou pelp Proprio (Carregue P)?";
             cin >> gerar;
         }
-        while(gerar!='A' || gerar!='a' || gerar!='P' || gerar!='p');
+        while(gerar!='A' && gerar!='a' && gerar!='P' && gerar!='p');
         if (gerar=='A' || gerar=='a')
             {
-                cliente.apostaautomatica(nchaves);
+                 cliente.apostaautomatica(nchaves);
             }
-            else if (gerar=='P' || gerar=='p')
+        else if (gerar=='P' || gerar=='p')
+        {
+            for (int i = 0; i < nchaves; i++)
             {
                 cout << "Insira" << "numeros que deseja apostar: ";
-                cin >> nnumap;
-                for (int i = 0; i < nnumap; i++)
+                for (int j = 0; j < numap; j++)
                 {
-                    cout << "Numero " << i+1 << ": ";
-                    cin >> numsapostados[i];
+                    cout << "Numero " << j+1 << ": ";
+                    cin >> numsapostados[j];
                 } 
+                cout << "Insira estrelas que deseja apostar:";
+                for (int k = 0; k < chaap; k++)
+                {
+                    cout << "Estrela " << k+1 << ": ";
+                    cin >> apestrela;
+                    estrelasapostados[k]+=apestrela;
+                }
+                cout << "Chave: ";
+                for (int a = 0; a < numap; a++)
+                {
+                    cout << numsapostados[a] << " ";
+                }
+
+                cout << "\nEstrelas: ";
+                for (int b = 0; b < chaap; b++)
+                {
+                    cout << estrelasapostados[b] << " ";
+                }
             }
-        
-        
-    }
+                    
+                    
+        }
+            
+            
+        }
     
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+                {
+                    cout << numsapostados[j] << " ";
+                }
+
+                cout << "\nEstrelas: ";
+                for (int k = 0; k < chaap; k++)
+                {
+                    cout << estrelasapostados[k] << " ";
+                }
