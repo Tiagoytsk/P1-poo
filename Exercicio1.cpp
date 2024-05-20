c45d46d3fb265e5487747a66171ace94593fa317
Tiagoytsk <tiagompedra@gmail.com>
2024-05-20 12:27:41 +0100

    final

--- Exercicio1.cpp
+++ Exercicio1.cpp
@@ -15,13 +15,13 @@ public:
     return abs(troco - aredondado) < 0.0001; 
     }
 
-    bool checknotas(float valorpago){
-    if (valorpago==5 || valorpago==10 || valorpago==20 || valorpago==50 || valorpago==100 || valorpago==200 || valorpago==500){
+    bool checknotas(int valorpago){
+    if (valorpago >= 5 && valorpago % 5 == 0){
         return true;
     }else{
         return false;
     }
-    }
+}
     double valor_pagamento(){
         random_device rd;
         default_random_engine generator(rd()); 
@@ -33,34 +33,6 @@ public:
 
     int trocof(float valorpagamento,float valorpago, float troco){
      cout << "\n\nTroco: " << troco;
-        if (troco>=500) {
-            cout << "\n\nNotas de 500 euros: " << trunc(troco/500);
-            troco=troco-500*(int)(troco/500);
-        }
-        if (troco>=200) {
-            cout << "\nNotas de 200 euros: " << trunc(troco/200);
-            troco=troco-200*(int)(troco/200);
-        }
-        if (troco>=100) {
-            cout << "\nNotas de 100 euros: " << trunc(troco/100);
-            troco=troco-100*(int)(troco/100);
-        }
-        if (troco>=50) {
-            cout << "\nNotas de 50 euros: " << trunc(troco/50);
-            troco=troco-50*(int)(troco/50);
-        }
-        if (troco>=20) {
-            cout << "\nNotas de 20 euros: " << trunc(troco/20);
-            troco=troco-20*(int)(troco/20);
-        }
-        if (troco>=10) {
-            cout << "\nNotas de 10 euros: " << trunc(troco/10);
-            troco=troco-10*(int)(troco/10);
-        }
-        if (troco>=5) {
-            cout << "\nNotas de 5 euros: " << trunc(troco/5);
-            troco=troco-5*(int)(troco/5);
-        }
         if (troco>=2) {
             cout << "\nMoedas de 2 euro: " << trunc(troco/2);
             troco=troco-2*(int)(troco/2);
@@ -107,7 +79,7 @@ public:
 //int pagamentos::trocof(float valorpagamento,float valorpago, float troco)
 
 int main() {
-    float valorpagamento, valorpago, troco;
+    double valorpagamento, valorpago, troco;
     valorpagamento = cliente.valor_pagamento();
     cout << "Caixa de Pagamento\n So sao aceites notas <5, 10, 20, 50, 100, 200 e 500 euros>:\n\nValor a pagar: "<< valorpagamento << "\n\n";
     if (cliente.temdoisdecimais(valorpagamento)==true)
--- Exercicio2.cpp
+++ Exercicio2.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <cmath>
 #include <random>
+#include <cstdlib>//para gerador de numeros
+#include <ctime>//
 using namespace std;
 
 
-class euromilhoes
-{
-private:
-    /* data */
+class Cliente {
 public:
-
-    int apostaautomatica(int nchaves){
-
+    void apostaautomaticanum(char tipoaposta){
+        cout<< "Chave: ";
+        int maxNums = (tipoaposta == 'S' || tipoaposta == 's') ? 5 : 11;
+        for (int i = 0; i < maxNums; i++) {
+            int num = rand() % 50 + 1; 
+            cout << num << ", ";
+        }
+        cout << endl;
     }
-    
+
+    void apostaautomaticastar(char tipoaposta){
+        cout<< "Estrelas: ";
+        int maxStars = (tipoaposta == 'S' || tipoaposta == 's') ? 2 : 9;
+        for (int i = 0; i < maxStars; i++) {
+            int star = rand() % 9 + 1;
+            cout << star << ", ";
+        }
+        cout<< endl;
+    }   
 }cliente;
 
 int main(){
@@ -41,7 +54,11 @@ int main(){
         while(gerar!='A' && gerar!='a' && gerar!='P' && gerar!='p');
         if (gerar=='A' || gerar=='a')
             {
-                 cliente.apostaautomatica(nchaves);
+            for (int t = 0; t < nchaves; t++)
+            {
+                cliente.apostaautomaticanum(tipoaposta);
+                cliente.apostaautomaticastar(tipoaposta);
+            }
             }
         else if (gerar=='P' || gerar=='p')
         {
@@ -78,5 +95,57 @@ int main(){
             
             
         }
-    
+    if(tipoaposta=='M'||tipoaposta=='m')
+    {
+        cout<<"Escolheu Aposta Multipla"<< endl <<"quantas boletins pretende apostar?";
+        cin >> nchaves;
+        do{
+        
+            cout << "Aposta Gerada Automaticamente (Carregue A) ou pelp Proprio (Carregue P)?";
+            cin >> gerar;
+        }
+        while(gerar!='A' && gerar!='a' && gerar!='P' && gerar!='p');
+        if (gerar=='A' || gerar=='a')
+        {
+            for (int t = 0; t < nchaves; t++)
+            {
+                cliente.apostaautomaticanum(tipoaposta);
+                cliente.apostaautomaticastar(tipoaposta);
+            }
+        }
+        else if (gerar=='P' || gerar=='p')
+        {
+            for (int i = 0; i < nchaves; i++)
+            {
+                cout << "Insira" << "numeros que deseja apostar: ";
+                for (int j = 0; j < numap; j++)
+                {
+                    cout << "Numero " << j+1 << ": ";
+                    cin >> numsapostados[j];
+                } 
+                cout << "Insira estrelas que deseja apostar:";
+                for (int k = 0; k < chaap; k++)
+                {
+                cout << "Estrela " << k+1 << ": ";
+                cin >> apestrela;
+                estrelasapostados[k] = apestrela; // use = instead of +=
+                }
+                cout << "Chave: ";
+                for (int a = 0; a < numap; a++)
+                {
+                    cout << numsapostados[a] << " ";
+                }
+
+                cout << "\nEstrelas: ";
+                for (int b = 0; b < chaap; b++)
+                {
+                    cout << estrelasapostados[b] << " ";
+                }
+            }
+                    
+                    
+        }
+            
+            
+        }
 }
