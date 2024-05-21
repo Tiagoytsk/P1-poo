ab7139346ddaf9f430963954f4b7e1b50d6aea00
Tiagoytsk <tiagompedra@gmail.com>
2024-05-21 20:32:05 +0100

    finalizado

--- Exercicio2.cpp
+++ Exercicio2.cpp
@@ -3,11 +3,14 @@
 #include <random>
 #include <cstdlib>//para gerador de numeros
 #include <ctime>//
+#include <cctype>
 using namespace std;
 
 
 class Cliente {
 public:
+    char tipoaposta, gerar,input[10];
+    int nchaves,apestrela,apnum, numap,chaap, numsapostados[10],estrelasapostados[2];
     void apostaautomaticanum(char tipoaposta){
         cout<< "Chave: ";
         int maxNums = (tipoaposta == 'S' || tipoaposta == 's') ? 5 : 11;
@@ -30,63 +33,118 @@ public:
 }cliente;
 
 int main(){
-    char tipoaposta, gerar;
-    int nchaves,apestrela,apnum, numap,chaap, numsapostados[10],estrelasapostados[2];
-    numap = 5;
-    chaap=2;
+    
+
+    cliente.numap = 5;
+    cliente.chaap=2;
 
     do {
         cout << "Bem vindo ao Euromilhões! \nEscolha o tipo de aposta que deseja realizar: \n Aposta Simples (carregue S) \n Aposta Multipla (carregue M)";
-        cin >> tipoaposta;
-        cout << tipoaposta;
+        cin >> cliente.tipoaposta;
+        cout << cliente.tipoaposta;
     } 
-    while (tipoaposta != 'S' && tipoaposta != 's' && tipoaposta != 'M' && tipoaposta != 'm');
+    while (cliente.tipoaposta != 'S' && cliente.tipoaposta != 's' && cliente.tipoaposta != 'M' && cliente.tipoaposta != 'm');
 
-    if (tipoaposta=='S' || tipoaposta=='s')
+    if (cliente.tipoaposta=='S' || cliente.tipoaposta=='s')
        {
-        cout << "Escolheu a Aposta Simples!\nQuantos boletins pertende apostar?";
-        cin >> nchaves;
+        do {
+            cout << "Escolheu a Aposta Simples!\nQuantos boletins pertende apostar (1-5)? ";
+            cin >> cliente.input;
+            if (std::isdigit(cliente.input[0]) && cliente.input[1] == '\0' && cliente.input[0] >= '1' && cliente.input[0] <= '5') {
+                cliente.nchaves = cliente.input[0] - '0';
+                break;
+            } else {
+                cout << "Erro!! Valor invalido insira um valor entre 1 - 5" << endl;
+            }
+    } while (true);
         do{
         
             cout << "Aposta Gerada Automaticamente (Carregue A) ou pelp Proprio (Carregue P)?";
-            cin >> gerar;
+            cin >> cliente.gerar;
         }
-        while(gerar!='A' && gerar!='a' && gerar!='P' && gerar!='p');
-        if (gerar=='A' || gerar=='a')
+        while(cliente.gerar!='A' && cliente.gerar!='a' && cliente.gerar!='P' && cliente.gerar!='p');
+        if (cliente.gerar=='A' || cliente.gerar=='a')
             {
-            for (int t = 0; t < nchaves; t++)
+            for (int t = 0; t < cliente.nchaves; t++)
             {
-                cliente.apostaautomaticanum(tipoaposta);
-                cliente.apostaautomaticastar(tipoaposta);
+                cliente.apostaautomaticanum(cliente.tipoaposta);
+                cliente.apostaautomaticastar(cliente.tipoaposta);
             }
             }
-        else if (gerar=='P' || gerar=='p')
+        else if (cliente.gerar=='P' || cliente.gerar=='p')
         {
-            for (int i = 0; i < nchaves; i++)
+            for (int i = 0; i < cliente.nchaves; i++)
             {
-                cout << "Insira" << "numeros que deseja apostar: ";
-                for (int j = 0; j < numap; j++)
-                {
-                    cout << "Numero " << j+1 << ": ";
-                    cin >> numsapostados[j];
-                } 
-                cout << "Insira estrelas que deseja apostar:";
-                for (int k = 0; k < chaap; k++)
-                {
-                    cout << "Estrela " << k+1 << ": ";
-                    cin >> apestrela;
-                    estrelasapostados[k]+=apestrela;
+                 cout << "Insira" << "numeros que deseja apostar: ";
+        cout << "Insira" << "numeros que deseja apostar: ";
+        for (int j = 0; j < cliente.numap; j++)
+        {
+            do {
+                cout << "Numero " << j+1 << ": ";
+                cin >> cliente.numsapostados[j];
+
+                if(cin.fail()) {
+                    cin.clear(); 
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    cout << "Invalid input. Please enter a number." << endl;
+                } else if (cliente.numsapostados[j] < 1 || cliente.numsapostados[j] > 50) {
+                    cout << "Invalid input. Please enter a number between 1 and 50." << endl;
+                } else {
+                    bool alreadyUsed = false;
+                    for (int i = 0; i < j; i++) {
+                        if (cliente.numsapostados[i] == cliente.numsapostados[j]) {
+                            alreadyUsed = true;
+                            break;
+                        }
+                    }
+                    if (alreadyUsed) {
+                        cout << "Invalid input. You've already used this number." << endl;
+                    } else {
+                        break;
+                    }
                 }
+            } while (true);
+        }
+        cout << "Insira estrelas que deseja apostar:";
+        for (int k = 0; k < cliente.chaap; k++)
+        {
+            do {
+                cout << "Estrela " << k+1 << ": ";
+                cin >> cliente.apestrela;
+
+                if(cin.fail()) {
+                    cin.clear(); 
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+                    cout << "Invalid input. Please enter a number." << endl;
+                } else if (cliente.apestrela < 1 || cliente.apestrela > 12) {
+                    cout << "Invalid input. Please enter a number between 1 and 12." << endl;
+                } else {
+                    bool alreadyUsed = false;
+                    for (int i = 0; i < k; i++) {
+                        if (cliente.estrelasapostados[i] == cliente.apestrela) {
+                            alreadyUsed = true;
+                            break;
+                        }
+                    }
+                    if (alreadyUsed) {
+                        cout << "Invalid input. You've already used this number." << endl;
+                    } else {
+                        cliente.estrelasapostados[k] = cliente.apestrela;
+                        break;
+                    }
+                }
+            } while (true);
+        }
                 cout << "Chave: ";
-                for (int a = 0; a < numap; a++)
+                for (int a = 0; a < cliente.numap; a++)
                 {
-                    cout << numsapostados[a] << " ";
+                    cout << cliente.numsapostados[a] << " ";
                 }
 
                 cout << "\nEstrelas: ";
-                for (int b = 0; b < chaap; b++)
+                for (int b = 0; b < cliente.chaap; b++)
                 {
-                    cout << estrelasapostados[b] << " ";
+                    cout << cliente.estrelasapostados[b] << " ";
                 }
             }
                     
@@ -95,51 +153,98 @@ int main(){
             
             
         }
-    if(tipoaposta=='M'||tipoaposta=='m')
+    if(cliente.tipoaposta=='M'||cliente.tipoaposta=='m')
     {
         cout<<"Escolheu Aposta Multipla"<< endl <<"quantas boletins pretende apostar?";
-        cin >> nchaves;
+        cin >> cliente.nchaves;
         do{
         
             cout << "Aposta Gerada Automaticamente (Carregue A) ou pelp Proprio (Carregue P)?";
-            cin >> gerar;
+            cin >> cliente.gerar;
         }
-        while(gerar!='A' && gerar!='a' && gerar!='P' && gerar!='p');
-        if (gerar=='A' || gerar=='a')
+        while(cliente.gerar!='A' && cliente.gerar!='a' && cliente.gerar!='P' && cliente.gerar!='p');
+        if (cliente.gerar=='A' || cliente.gerar=='a')
         {
-            for (int t = 0; t < nchaves; t++)
+            for (int t = 0; t < cliente.nchaves; t++)
             {
-                cliente.apostaautomaticanum(tipoaposta);
-                cliente.apostaautomaticastar(tipoaposta);
+                cliente.apostaautomaticanum(cliente.tipoaposta);
+                cliente.apostaautomaticastar(cliente.tipoaposta);
             }
         }
-        else if (gerar=='P' || gerar=='p')
+        else if (cliente.gerar=='P' || cliente.gerar=='p')
         {
-            for (int i = 0; i < nchaves; i++)
+                     for (int i = 0; i < cliente.nchaves; i++)
             {
-                cout << "Insira" << "numeros que deseja apostar: ";
-                for (int j = 0; j < numap; j++)
-                {
-                    cout << "Numero " << j+1 << ": ";
-                    cin >> numsapostados[j];
-                } 
-                cout << "Insira estrelas que deseja apostar:";
-                for (int k = 0; k < chaap; k++)
-                {
-                cout << "Estrela " << k+1 << ": ";
-                cin >> apestrela;
-                estrelasapostados[k] = apestrela; // use = instead of +=
+                 cout << "Insira" << "numeros que deseja apostar: ";
+        cout << "Insira" << "numeros que deseja apostar: ";
+        for (int j = 0; j < cliente.numap; j++)
+        {
+            do {
+                cout << "Numero " << j+1 << ": ";
+                cin >> cliente.numsapostados[j];
+
+                if(cin.fail()) {
+                    cin.clear(); 
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    cout << "Invalid input. Please enter a number." << endl;
+                } else if (cliente.numsapostados[j] < 1 || cliente.numsapostados[j] > 50) {
+                    cout << "Invalid input. Please enter a number between 1 and 50." << endl;
+                } else {
+                    bool alreadyUsed = false;
+                    for (int i = 0; i < j; i++) {
+                        if (cliente.numsapostados[i] == cliente.numsapostados[j]) {
+                            alreadyUsed = true;
+                            break;
+                        }
+                    }
+                    if (alreadyUsed) {
+                        cout << "Invalid input. You've already used this number." << endl;
+                    } else {
+                        break;
+                    }
                 }
+            } while (true);
+        }
+        cout << "Insira estrelas que deseja apostar:";
+        for (int k = 0; k < cliente.chaap; k++)
+        {
+            do {
+                cout << "Estrela " << k+1 << ": ";
+                cin >> cliente.apestrela;
+
+                if(cin.fail()) {
+                    cin.clear(); 
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+                    cout << "Invalid input. Please enter a number." << endl;
+                } else if (cliente.apestrela < 1 || cliente.apestrela > 12) {
+                    cout << "Invalid input. Please enter a number between 1 and 12." << endl;
+                } else {
+                    bool alreadyUsed = false;
+                    for (int i = 0; i < k; i++) {
+                        if (cliente.estrelasapostados[i] == cliente.apestrela) {
+                            alreadyUsed = true;
+                            break;
+                        }
+                    }
+                    if (alreadyUsed) {
+                        cout << "Invalid input. You've already used this number." << endl;
+                    } else {
+                        cliente.estrelasapostados[k] = cliente.apestrela;
+                        break;
+                    }
+                }
+            } while (true);
+        }   
                 cout << "Chave: ";
-                for (int a = 0; a < numap; a++)
+                for (int a = 0; a < cliente.numap; a++)
                 {
-                    cout << numsapostados[a] << " ";
+                    cout << cliente.numsapostados[a] << " ";
                 }
 
                 cout << "\nEstrelas: ";
-                for (int b = 0; b < chaap; b++)
+                for (int b = 0; b < cliente.chaap; b++)
                 {
-                    cout << estrelasapostados[b] << " ";
+                    cout << cliente.estrelasapostados[b] << " ";
                 }
             }
                     
